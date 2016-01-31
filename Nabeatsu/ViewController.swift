3adcdf72468885dc8fa48e4789c10f6b4c86f7aa
Fujii Yosuke <fujiyou1121@gmail.com>
2016-02-01 00:22:04 +0900

    Initial Commit

---
+++ Nabeatsu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nabeatsu
+//
+//  Created by 藤井陽介 on 2016/02/01.
+//  Copyright © 2016年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

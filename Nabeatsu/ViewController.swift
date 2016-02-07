5b4c4ae8e52a136245cc5219ddcd9a45460353fa
Fujii Yosuke <fujiyou1121@gmail.com>
2016-02-07 17:10:52 +0900

    add README.md

--- Nabeatsu/ViewController.swift
+++ Nabeatsu/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    @IBOutlet var faceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func isAho() -> Bool {
+        if number % 3 == 0 {
+            return true
+        }
+        if String(number).containsString("3") {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func push() {
+        number++
+        label.text = String(number)
+        if isAho() {
+            faceLabel.text = "ლ(´ڡ`ლ)"
+        } else {
+            faceLabel.text = "(*´ω｀*)"
+        }
+    }
 
-
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = "\(0)"
+        faceLabel.text = "(*´ω｀*)"
+    }
 }
 
